0dc7cc41057699761f3023d02a7de71c7dfc730b
dje144 <dje144@g.harvard.edu>
2017-05-20 00:47:34 -0400

    fixes

--- lib/configmgmt-scripts/checkpoint.php
+++ lib/configmgmt-scripts/checkpoint.php
@@ -265,7 +265,8 @@ function runCollector($device, $saveToFolder, $saveToFile, $username, $password,
     $strStart = date("F d, Y h:m:s", $stime);
     $strEnd = date("F d, Y h:m:s", $etime);
     $cmd = 'fw log -l -n -p -z -b "' . $strStart . '" "' . $strEnd . '" | egrep -o "rule: [0-9]*|NAT_rulenum: [0-9]*" | awk \'{count[$1,$2]++} END {for (word in count) print word, count[word]}\'';
-    $cmd2 = '/bin/cpfw_start log -l -n -p -z -b "' . $strStart . '" "' . $strEnd . '" | egrep -o "rule: [0-9]*|NAT_rulenum: [0-9]*" | awk \'{count[$1,$2]++} END {for (word in count) print word, count[word]}\'';
+    //$cmd2 = '/bin/cpfw_start log -l -n -p -z -b "' . $strStart . '" "' . $strEnd . '" | egrep -o "rule: [0-9]*|NAT_rulenum: [0-9]*" | awk \'{count[$1,$2]++} END {for (word in count) print word, count[word]}\'';
+    $cmd2 = '/opt/' . $ver . '/fw1/bin/fw log -l -n -p -z -b "' . $strStart . '" "' . $strEnd . '" | egrep -o "rule: [0-9]*|NAT_rulenum: [0-9]*" | awk \'{count[$1,$2]++} END {for (word in count) print word, count[word]}\'';
 
     outputText("Collecting hit count from $strStart to $strEnd");
 
